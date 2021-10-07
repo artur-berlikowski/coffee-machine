e37ca1599133df474df3a1c98900dba1ef15c12f
arturberlikowski <a.berlikowski@gmail.com>
2021-10-07 11:36:08 +0200

    Added Caffe Latte to the menu of drink options.

--- src/Drink.java
+++ src/Drink.java
@@ -12,7 +12,14 @@ public enum Drink {
         put(Ingredient.MILK, 0.25);
     }}),
 
-    COFFEE_ICE("Coffee (Ice)", 25, new HashMap<>(){{
+    COFFEE_LATTE("Caffe Latte", 32, new HashMap<>(){{
+        put(Ingredient.COFFEE, 0.10);
+        put(Ingredient.WATER, 0.25);
+        put(Ingredient.MILK, 0.75);
+        put(Ingredient.SUGAR, 0.05);
+    }}),
+
+    COFFEE_ICE("Coffee (Ice)", 28, new HashMap<>(){{
         put(Ingredient.COFFEE, 0.05);
         put(Ingredient.WATER, 0.75);
         put(Ingredient.MILK, 0.20);
